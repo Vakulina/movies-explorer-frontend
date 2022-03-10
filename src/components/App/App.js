10d6856fe60172290387adf52c0feed267728dab
Vakulina <viktoria.ganina@yandex.ru>
2022-03-10 14:18:54 +0300

    сделать роутинг

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
@@ -9,12 +10,26 @@ import './App.css';
 function App() {
   return (
     <div className="page">
-<Main />
-<Movies />
-<SavedMovies/>
-<Register/>
-<Login />
-<Profile />
+      <Routes>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Routes>
     </div>
   );
 }
