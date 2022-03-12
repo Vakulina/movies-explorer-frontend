1705fcb9756465a125c759010ba37b781cf7a281
Vakulina <viktoria.ganina@yandex.ru>
2022-03-12 21:17:04 +0300

    начать верстать список технологий

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import './Main.css';
 import Promo from '../Promo/Promo'
 import Header from '../Header/Header';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
 <Header isLogin={false}/>
 <Promo/>
 <AboutProject/>
+<Techs />
 </main>
   );
 }
