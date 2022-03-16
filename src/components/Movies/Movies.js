18dfa2c28531e60eb2fa7a602ce007f9ab498cc7
Vakulina <viktoria.ganina@yandex.ru>
2022-03-16 14:32:02 +0300

    movies на всех разрешениях

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,14 +2,17 @@ import './Movies.css';
 import Header from '../Header/Header';
 import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
-import MoviesCardList from '../MoviesCardList/MoviesCardList'
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
 export default function Movies({isLoading}) {
   return (
-<>
+<section className='movie'>
 <Header isLogin={true} />
 {isLoading&&<Preloader />}
 <SearchForm />
 <MoviesCardList typeList='search-movies'/>
-</>
+<button className='movie__more-btn'>Ещё</button>
+<Footer />
+</section>
   );
 }
