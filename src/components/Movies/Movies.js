4f19e963c6bdcf69eeb906759b52cdda580cf08a
Vakulina <viktoria.ganina@yandex.ru>
2022-03-16 15:12:20 +0300

    сверстать SavedMovies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,15 +4,15 @@ import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
-export default function Movies({isLoading}) {
+export default function Movies({ isLoading }) {
   return (
-<section className='movie'>
-<Header isLogin={true} />
-{isLoading&&<Preloader />}
-<SearchForm />
-<MoviesCardList typeList='search-movies'/>
-<button className='movie__more-btn'>Ещё</button>
-<Footer />
-</section>
+    <section className='movie'>
+      <Header isLogin={true} />
+      {isLoading && <Preloader />}
+      <SearchForm />
+      <MoviesCardList typeList='search-movies' />
+      <button className='movie__more-btn'>Ещё</button>
+      <Footer />
+    </section>
   );
 }
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,8 +1,18 @@
 import './SavedMovies.css';
+import Header from '../Header/Header';
+import Preloader from '../Preloader/Preloader';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
 
-export default function SavedMovies() {
+export default function SavedMovies({ isLoading }) {
   return (
-<>
-</>
+    <section className='saved-movie'>
+      <Header isLogin={true} />
+      {isLoading && <Preloader />}
+      <SearchForm />
+      <MoviesCardList typeList='saved-movies' />
+      <Footer />
+    </section>
   );
 }
