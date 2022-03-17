7cf9185b5a08f3d1df4beb89a309079671efa5fa
Vakulina <viktoria.ganina@yandex.ru>
2022-03-17 18:59:51 +0300

    подготовка к публикации

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,10 @@
 import './Login.css';
 import logo from '../../images/logo__header.svg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const mail = 'pochta@yandex.ru';
+  const navigate = useNavigate();
   return (
     <section className='login'>
       <Link className="login__link login__link_logo" to='/'>
@@ -14,16 +15,16 @@ export default function Login() {
         <div className='login__fields'>
           <div className='login__field'>
             <label className='login__label' htmlFor='mail'>E-mail</label>
-            <input className='login__input' type='email' placeholder={mail} id='mail' required />
+            <input className='login__input' type='email' placeholder={mail} id='mail' />
           </div>
           <div className='login__field'>
             <label className='login__label' htmlFor='password'>Пароль</label>
-            <input className='login__input' type='password' id='password' required />
+            <input className='login__input' type='password' id='password' />
           </div>
           <span className='login__error'></span>
         </div>
         <div className='login__buttons'>
-          <button className='login__button login__button_enter'>Войти</button>
+          <button className='login__button login__button_enter' onClick={()=>navigate('/movies')}>Войти</button>
           <span className='login__sign'>Еще не зарегистрированы?</span>
           <Link to="/signup" className="login__link login__link_registration">
             Регистрация
