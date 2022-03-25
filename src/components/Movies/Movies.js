f0201105e4f01490df4ade40677e2125ca1b468a
Vakulina <viktoria.ganina@yandex.ru>
2022-03-25 20:52:02 +0300

    запрос к внешнему API

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,15 +1,33 @@
 import './Movies.css';
+import { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
-export default function Movies({ isLoading }) {
+import { moviesApi } from '../../utils/MoviesApi';
+import { mainApi } from '../../utils/MainApi';
+
+export default function Movies() {
+const [isLoading, setLoadingStatus] = useState(false);
+const [error, setError] = useState('');
+  useEffect(()=>{
+    setLoadingStatus(true)
+    moviesApi.getMovies() 
+    .then((res)=>{
+      console.log(res)
+      setLoadingStatus(false)
+    })
+    .catch((err)=> setError(err.message))
+  .finally(()=>setLoadingStatus(false))
+  },
+  [])
   return (
     <section className='movie'>
       <Header isLogin={true} />
-      {isLoading && <Preloader />}
       <SearchForm typeList='search-movies'/>
+      {isLoading && <Preloader />}
+      {error.length &&  <span className='search-movies__error'>{error}</span>}
       <MoviesCardList typeList='search-movies' />
       <button className='movie__more-btn'>Ещё</button>
       <Footer />
--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,5 +1,6 @@
+import {DEFAULT_ERROR_MESSAGE} from './constants'
 const configConnection = {
-  url: 'https://api.nomoreparties.co/beatfilm-movies',
+  url: 'https://api.nomoreparties.co/beatfilm-movies-1',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -10,6 +11,24 @@ class MoviesApi {
     this._url = config.url;
     this._headers = config.headers;
   }
+
+  _checkRequest(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return res.json()
+        .then(() => {
+          return Promise.reject(new Error(DEFAULT_ERROR_MESSAGE));
+        });
+    }
+  }
+  getMovies() {
+    return fetch(this._url, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._checkRequest);
+  }
+
 
 }
 
